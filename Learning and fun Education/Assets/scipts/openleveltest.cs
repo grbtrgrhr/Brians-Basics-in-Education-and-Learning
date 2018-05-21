ecb3495f951698a56eba6503062408ff7de5a382
Daniel <gamerpcx@outlook.com>
2018-05-21 10:19:59 -0700

    rbrreggbeg
    gegeg

---
+++ Learning and fun Education/Assets/scipts/openleveltest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class openleveltest : MonoBehaviour
+{
+    public string level = "testingscene2";
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision Colider)
+    {
+        if (Colider.gameObject.tag == "Player") ;
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(level);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
