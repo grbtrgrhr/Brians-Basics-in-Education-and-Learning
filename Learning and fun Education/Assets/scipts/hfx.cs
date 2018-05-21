3ae0f5d543a2c57a6d934e21b171515ffd50f477
Daniel <gamerpcx@outlook.com>
2018-05-21 12:24:56 -0700

    fff
    kkk

--- Learning and fun Education/Assets/scipts/hfx.cs
+++ Learning and fun Education/Assets/scipts/hfx.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class openleveltest : MonoBehaviour
+public class hfx : MonoBehaviour
 {
-    public string level = "BUS";
+    public string level = "GYM";
 
     // Use this for initialization
     void OnCollisionEnter(Collision Colider)
     {
         if (Colider.gameObject.tag == "Player") ;
 #pragma warning disable CS0618 // Type or member is obsolete
-        Application.LoadLevel(5);
+        Application.LoadLevel(4);
 #pragma warning restore CS0618 // Type or member is obsolete
     }
 }
