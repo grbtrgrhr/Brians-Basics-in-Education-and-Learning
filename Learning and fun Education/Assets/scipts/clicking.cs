9d2a7df3e5cce4132bb064f5001a94697fe997e4
Daniel <gamerpcx@outlook.com>
2018-05-26 10:04:14 -0700

    ooooo
    oooooooooooooooooo

---
+++ Learning and fun Education/Assets/scipts/clicking.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class clicking : MonoBehaviour
+{
+    public string anylevel;
+
+     public void Button_Click()
+    {
+        Application.LoadLevel("menu");
+    }
+
+	
+}
