c4cfb1d98258f71481953c2389d48514d069a30b
Daniel <gamerpcx@outlook.com>
2018-05-26 11:41:17 -0700

    frfre
    feefef

---
+++ Learning and fun Education/Assets/scipts/Loadlevel1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadlevel1 : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider ChangeScene) // can be Collider HardDick if you want.. I'm not judging you
+    {
+        if (ChangeScene.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevelAdditive(6); //1 is the build order it could be 1065 for you if you have that many scenes
+        }
+    }
+
+}
