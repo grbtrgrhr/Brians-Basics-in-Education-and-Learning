4202ac0255fa003ead64189b39596ef9d352d28e
Daniel <gamerpcx@outlook.com>
2018-05-26 12:13:21 -0700

    TEST
    TEST

---
+++ Learning and fun Education/Assets/scipts/Gymopenleveltest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gymopenleveltest : MonoBehaviour
+{ 
+
+    public string level = "GYM";
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision Colider)
+    {
+        if (Colider.gameObject.tag == "Player") ;
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(4);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
---
+++ Learning and fun Education/Assets/scipts/schoolopenlevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class schoolopenlevel : MonoBehaviour
+{ 
+
+    public string level = "School";
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision Colider)
+    {
+        if (Colider.gameObject.tag == "Player") ;
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(2);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
