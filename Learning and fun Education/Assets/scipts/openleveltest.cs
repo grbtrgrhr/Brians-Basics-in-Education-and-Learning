4e121e5b6a029db71c68445c3e5331a065567c21
Daniel <gamerpcx@outlook.com>
2018-05-26 12:42:22 -0700

    wfwfwf
    wfwf

--- Learning and fun Education/Assets/scipts/openleveltest.cs
+++ Learning and fun Education/Assets/scipts/openleveltest.cs
@@ -11,7 +11,7 @@ public class openleveltest : MonoBehaviour
     {
         if (Colider.gameObject.tag == "Player") ;
 #pragma warning disable CS0618 // Type or member is obsolete
-        Application.LoadLevel(5);
+        Application.LoadLevel(4);
 #pragma warning restore CS0618 // Type or member is obsolete
     }
 }
